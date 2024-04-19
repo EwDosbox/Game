b0aa6afe6c5db1772c882615e7fbdf2bba21d9cb
EwDosbox <cada.vviktor@gmail.com>
2024-04-19 12:24:48 +0200

    Přidejte soubory projektu.

---
+++ Game.BackEnd/HerniPostava.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.BackEnd
+{
+    public class HerniPostava
+    {
+        public string test = "sd";
+    }
+}
